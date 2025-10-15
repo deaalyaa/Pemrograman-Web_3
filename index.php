d29cafd73df06139b06f1ef810d582ed4f1ac01e
dea alya <Deaalya675@gmail.com>
2025-10-15 20:28:50 +0700

    memasukan folder

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Beranda</title>
+    <link rel="stylesheet" href="asets/style.css"  href="style.css">
+</head>
+<body>
+    <div id="wrapper">
+		<div id ="container">
+		<header><img src="img/header.png"></header>
+		<nav>
+			<ul>
+				<li><a href="index.php">Beranda</a></li>
+				<li><a href="#">Artikel</a></li>
+				<li><a href="#">Profil</a></li>
+					<ul class="submenu">
+						<li><a href="#">Hub Kami</a></li>						
+					</ul>
+				</li>
+			</ul>
+		</nav>
+		<article>
+			<div id="single">
+				<div id="t-images" style="background:url(img/article-1.jpg)">apani;
+				</div>
+				<h2>BELAJAR WEB DENGAN HTML+CSS3</H2>
+				Leonard memberikan contoh kendaraan yang memiliki desain jok sederhana, seperti Daihatsu Gran Max, Toyota Kijang lama, dan Suzuki Carry. Ia menyatakan bahwa mobil-mobil tersebut tetap terlihat rapi hanya dengan menggunakan sarung jok. “Model joknya itu rata, glat, sarung saja bisa rapi. Pundaknya juga rata. Coba kalau Agya saja nih yang sekarang, dia melengkung,” tambahnya.
+				<div id="more"><a href="article1.php">Baca Selengkapnya</a></div>
+			</div>
+				<div id="t-images" style="background:url(img/article-2.)">&nbsp;
+				</div>
+				<h2>BELAJAR WEB DENGAN HTML+CSS3</H2>
+				Leonard memberikan contoh kendaraan yang memiliki desain jok sederhana, seperti Daihatsu Gran Max, Toyota Kijang lama, dan Suzuki Carry. Ia menyatakan bahwa mobil-mobil tersebut tetap terlihat rapi hanya dengan menggunakan sarung jok. “Model joknya itu rata, glat, sarung saja bisa rapi. Pundaknya juga rata. Coba kalau Agya saja nih yang sekarang, dia melengkung,” tambahnya.
+				<div id="more"><a href="article11.php">Baca Selengkapnya</a></div>
