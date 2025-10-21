87fb55e89870e34e41b96438cfd7bf017750d8ee
dea alya <Deaalya675@gmail.com>
2025-10-22 00:08:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DeaNews</title>
+    <link rel="stylesheet" href="asets/css/main.css">
+</head>
+<body>
+    <div id="wrapper">
+      <div id="container">            
+            <header><img src="asets/img/Header1.png"></header>            
+            <nav>
+				<ul>
+					<li><a href="index.php" class="active">Beranda</a></li>
+					<li><a href="article/berita.php">Article</a></li>
+					<li><a href="article/profil.php">Profil</a>
+						<ul class="submenu">
+							<li><a href="article/profil.php">Hub Kami</a></li>
+							<li><a href="article/profil.php">Tentang Kami</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>            
+            <article>
+				<div id="single">
+					<div id="t-image" style="background:url(asets/img/1.jpg)">&nbsp;</div>
+					<h2>Indonesia mendidih, BMKG: Hindari keluar rumah pukul 10.00-16.00</h2>
+					Badan Meteorologi, Klimatologi, dan Geofisika (BMKG) mengimbau masyarakat agar menghindari aktivitas di luar ruangan pada pukul 10.00–16.00 WIB selama periode cuaca panas ekstrem yang tengah melanda berbagai wilayah Indonesia.Deputi Bidang Meteorologi BMKG, Guswanto, menjelaskan bahwa cuaca panas ekstrem diperkirakan masih akan berlangsung hingga akhir Oktober atau awal November 2025 .<div id="more"><a href="article/article1.php">Baca selengkapnya</a></div>
+				</div>
+				<div id="single">
+					<div id="t-image" style="background:url(asets/img/article2.jpg)">&nbsp;</div>
+					<h2>Setahun pemerintahan Prabowo, posisi PDIP masih jadi pertanyaan</h2>
+					Ketika Prabowo Subianto dilantik menjadi presiden kedelapan Indonesia pada 2024, Partai Demokrasi Indonesia Perjuangan (PDIP) tidak ingin disebut sebagai oposisi, melainkan "partai penyeimbang".
+					PDIP yang menguasai 110 dari 580 kursi di DPR adalah satu dari dua partai politik (parpol) yang tidak masuk dalam koalisi pemerintahan Prabowo. Partai lainnya di luar koalisi adalah Nasional Demokrat (NasDem) dengan 69 kursi di parlemen.<div id="more"><a href="article/article2.php">Baca selengkapnya</a></div>
+				</div>
+				<div id="single">
+					<div id="t-image" style="background:url(asets/img/article3.jpg)">&nbsp;</div>
+					<h2>Kronologi kematian Timothy Anugerah dan dugaan bullying di Universitas Udayana</h2>
+					Seorang mahasiswa Sosiologi FISIP Universitas Udayana (Unud), Timothy Anugerah Saputra, meninggal dunia pada Rabu (15/10), setelah terjun dari gedung FISIP di Denpasar, Bali. Insiden ini memantik gelombang reaksi publik usai beredar tangkapan layar percakapan di media sosial yang menyinggung kematiannya dengan nada mengejek, memantik dugaan adanya perundungan di balik dinding kampus. <div id="more"><a href="article/article3.php">Baca selengkapnya</a></div>
+				</div>
+				</div>
+			</article>             
+            <aside>
+					<li class="widget">
+						<h2>Berita Populer</h2>
+						<ul>
+						<li><a href="article/article3.php">Kematian Timothy Anughrah</a></li>
+						<li><a href="article/article2.php">Mempertanyakan posisi PDIP</a></li>
+						<li><a href="article/article1.php">Hindari Keluar Rumah Pukul 10.00-16.00</a></li>
+						</ul>
+					</li>
+					<li class="widget">
+						<h2>Artikel Populer</h2>
+						<ul>
+						<li><a href="arpop1.php">Artikel 1</a></li>
+						<li><a href="arpop2.php">Artikel 2</a></li>
+						<li><a href="arpop3.php">Artikel 3</a></li>
+						</ul>
+					</li>
+					<li class="widget">
+						<h2>Berita selanjutnya</h2>
+						<ul>
+						<li><a href="bersel1.php">Gejala pusing katena kuliah</a></li>
+						<li><a href="bersel2.php">Kapan Libur Anak Kuliahan</a></li>
+						<li><a href="besel3.php">Prediksi Cuaca Hari ini</a></li>
+						</ul>
+					</li>
+			</aside>       			
+           <footer>
+				<div id="column">
+					<h2>LAYANAN</h2>
+					E-Mail<br>
+					E-Learning<br>
+					E-Repository<br>
+					E-Jurnal<br>
+					Digital Library<br>
+					Portal SIA<br>
+					SI-Selma<br>
+					SI-Dahlia
+				</div>
+				<div id="column">
+					<h2>Business Hour</h2>
+					Silahkan hubungi pusbangnis UINSU telp 064123123 pada jam kerja atau email <a href="#">pusbangnis@uinsu.ac.id</a>
+				</div>
+				<div id="column">
+					<h2>Head Office</h2>
+					Alamat Jl. Willem Iskandar, Pasar V, Medan Estate
+					Tel : +62-61-6615683
+					Email : humas@uinsu.ac.id
+					
+				</div>
+			</footer>           
+        </div>
+	</div>
+</body>
+</html>
