6b9ac608324a260dfdea7c6f7f3f5bda0454cd54
dea alya <Deaalya675@gmail.com>
2025-10-21 15:37:28 +0700

    Add files via upload

---
+++ Article/article3.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DeaNews</title>
+    <link rel="stylesheet" href="../asets/css/main.css">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="container">            
+            <header><img src="../asets/img/Header1.png"></header>            
+            <nav>
+				<ul>
+					<li><a href="../index.php">Beranda</a></li>
+					<li><a href="./berita.php">Article</a></li>
+					<li><a href="profil.php">Profil</a>
+						<ul class="submenu">
+							<li><a href="#">Hub Kami</a></li>
+							<li><a href="#">Tentang Kami</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav> 
+			<article>
+				<div id="single">
+					<div id="t-image" style="background:url(../asets/img/article3.jpg)">&nbsp;</div>
+					<h2>Indonesia mendidih, BMKG: Hindari keluar rumah pukul 10.00-16.00</h2>
+					<p>JAKARTA: Badan Meteorologi, Klimatologi, dan Geofisika (BMKG) mengimbau masyarakat agar menghindari aktivitas di luar ruangan pada pukul 10.00–16.00 WIB selama periode cuaca panas ekstrem yang tengah melanda berbagai wilayah Indonesia</P>
+					<p>Deputi Bidang Meteorologi BMKG, Guswanto, menjelaskan bahwa cuaca panas ekstrem diperkirakan masih akan berlangsung hingga akhir Oktober atau awal November 2025.</p>
+					<p> “Fenomena cuaca ini disebabkan oleh kombinasi gerak semu matahari dan pengaruh monsun Australia,” ujar Guswanto.</p>
+					<h4>CUACA PANAS EKSTREM DAN RISIKO KESEHATAN</h4>
+					<p>Direktur Meteorologi Publik BMKG, Andri Ramdhani, menegaskan bahwa masyarakat perlu berhati-hati karena paparan sinar matahari pada jam-jam tertentu dapat menimbulkan risiko kesehatan serius.
+					<p>“Warga diimbau untuk mengurangi aktivitas di luar ruangan pada pukul 10.00–16.00 WIB karena cuaca panas di sejumlah wilayah berpotensi membahayakan kesehatan,” kata Andri, dikutip dari Kompas.com, Sabtu (18/10).
+					<p>Berdasarkan hasil pengamatan BMKG, indeks sinar ultraviolet (UV) di beberapa wilayah Indonesia kini berada pada level berisiko tinggi, terutama ketika paparan sinar matahari berlangsung lama. 
+					<p>Dalam beberapa hari terakhir, BMKG mencatat suhu maksimum di siang hari mencapai 38 derajat Celsius, kondisi yang dapat memicu heatstroke atau kelelahan akibat panas berlebih.</p>
+				</div>
+			</article>             
+            <aside>
+					<li class="widget">
+						<h2>Berita Populer</h2>
+						<ul>
+						<li><a href="./article3.php">Kematian Timothy Anughrah</a></li>
+						<li><a href="./article2.php">Mempertanyakan posisi PDIP</a></li>
+						<li><a href="./article1.php">Hindari Keluar Rumah Pukul 10.00-16.00</a></li>
+						</ul>
+					</li>
+					<li class="widget">
+						<h2>Artikel Populer</h2>
+						<ul>
+						<li><a href="#">Artikel 1</a></li>
+						<li><a href="#">Artikel 2</a></li>
+						<li><a href="#">Artikel 3</a></li>
+						</ul>
+					</li>
+					<li class="widget">
+						<h2>Berita selanjutnya</h2>
+						<ul>
+						<li><a href="#">Gejala pusing katena kuliah</a></li>
+						<li><a href="#">Kapan Libur Anak Kuliahan</a></li>
+						<li><a href="#">Prediksi Cuaca Hari ini</a></li>
+						</ul>
+					</li>
+			</aside>            
+            <footer>
+				<div id="column">
+					<h2>LAYANAN</h2>
+					E-Mail<br>
+					E-Learning<br>
+					E-Repository<br>
+					E-Jurnal<br>
+					Digital Library<br>
+					Portal SIA<br>
+					SI-Selma<br>
+					SI-Dahlia
+				</div>
+				<div id="column">
+					<h2>Business Hour</h2>
+					Silahkan hubungi pusbangnis UINSU telp 064123123 pada jam kerja atau email <a href="#">pusbangnis@uinsu.ac.id</a>
+				</div>
+				<div id="column">
+					<h2>Head Office</h2>
+					Alamat Jl. Willem Iskandar, Pasar V, Medan Estate
+					Tel : +62-61-6615683
+					Email : humas@uinsu.ac.id
+					
+				</div>
+			</footer>           
+        </div>
+	</div>
+</body>
+</html>
---
+++ Article/profil.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DeaNews</title>
+    <link rel="stylesheet" href="../asets/css/main.css">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="container">            
+            <header><img src="../asets/img/Header1.png"></header>            
+            <nav>
+				<ul>
+					<li><a href="../index.php">Beranda</a></li>
+					<li><a href="./berita.php">Article</a></li>
+					<li><a href="profil.php" class="active">Profil</a>
+						<ul class="submenu">
+							<li><a href="#hub">Hub Kami</a></li>
+							<li><a href="#tentang">Tentang Kami</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav> 
+			 <section id="profil">
+            <h1>Profil DeaNews</h1>
+            <p>DeaNews adalah portal berita indonesia yang menyajikan informasi terkini seputar dunia pahit, sosial, dan masyarakat di lingkungan kampus dan sekitar.</p>
+        </section>
+        <section id="hub">
+            <h2>Hubungi Kami</h2>
+            <ul>
+                <li>Email: deanews@uinsu.ac.id</li>
+                <li>Telepon: +62 838 3517 8502</li>
+                <li>Instagram: deaawly_</li>
+            </ul>
+        </section>
+        <section id="tentang">
+            <h2>Tentang Kami</h2>
+            <p>Kami hadir untuk memberikan informasi terpercaya dan edukatif bagi masyarakat akademik dan umum, dengan dedikasi penuh demi perkembangan literasi di Indonesia.</p>
+        </section>
+			 <footer>
+				<div id="column">
+					<h2>LAYANAN</h2>
+					E-Mail<br>
+					E-Learning<br>
+					E-Repository<br>
+					E-Jurnal<br>
+					Digital Library<br>
+					Portal SIA<br>
+					SI-Selma<br>
+					SI-Dahlia
+				</div>
+				<div id="column">
+					<h2>Business Hour</h2>
+					Silahkan hubungi pusbangnis UINSU telp 064123123 pada jam kerja atau email <a href="#">pusbangnis@uinsu.ac.id</a>
+				</div>
+				<div id="column">
+					<h2>Head Office</h2>
+					Alamat Jl. Willem Iskandar, Pasar V, Medan Estate
+					Tel : +62-61-6615683
+					Email : humas@uinsu.ac.id
+					
+				</div>
+			</footer>           
+        </div>
+	</div>
+</body>
+</html>
